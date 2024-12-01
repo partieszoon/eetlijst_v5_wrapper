a329a727d55129f1fa6c8eaa010643c9247e3af9
Joeri Geuzinge <r.j.geuzinge@student.tudelft.nl>
2024-12-01 01:39:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "certifi==2024.8.30"
-        "charset-normalizer==3.4.0"
-        "idna==3.10"
-        "requests==2.32.3"
+        "certifi==2024.8.30",
+        "charset-normalizer==3.4.0",
+        "idna==3.10",
+        "requests==2.32.3",
         "urllib3==2.2.3"
-
     ],
     package_data={
         'eetlijst_v5_wrapper': [
